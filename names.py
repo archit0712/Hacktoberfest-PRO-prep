c5483071754d0d9d38dcd2ad8b982bd5353569d8
Aaryan-Kapur <aaryankapur1309@gmail.com>
2020-10-11 21:15:26 +0530

    Create names.py

---
+++ names.py
@@ -0,0 +1,24 @@
+print("Aaryan Kapur
+Developer, entrepeneur with a knack for problem solving.
+aaryankapur.tech")
+
+print("Abhinav Robinson
+Developer|Designer and a ~music listening hobbist?~.")
+
+print("Karthikeyan Rathore
+Developer|Love Hacking and Video editing.")
+
+print("Daksh Jain
+Developer| Technical Consultant and an ardent fan of Real Madrid :)
+DakshJn")
+
+print("Dakshita agarwal
+Developer| Technical writer and Bug bounty hunter")
+
+print("venkatesh pensalwar
+Developer| Love to learn new things and share knowledge.
+venkateshpensalwar")
+
+print("Aniket Chowdhury
+Developer| Machine Learning Analyst
+aniketbiprojit")
