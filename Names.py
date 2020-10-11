d650e4fb9f4f1e196fab2185860abcfcbae31f9f
Aaryan-Kapur <aaryankapur1309@gmail.com>
2020-10-11 21:23:21 +0530

    Update Names.py

--- Names.py
+++ Names.py
@@ -1,27 +1,15 @@
-print("Aaryan Kapur
-Developer, entrepeneur with a knack for problem solving.
-aaryankapur.tech")
+print("Aaryan Kapur Developer, entrepeneur with a knack for problem solving. aaryankapur.tech")
 
-print("Abhinav Robinson
-Developer|Designer and a ~music listening hobbist?~.")
+print("Abhinav Robinson Developer|Designer and a ~music listening hobbist?~.")
 
-print("Karthikeyan Rathore
-Developer|Love Hacking and Video editing.")
+print("Karthikeyan Rathore Developer|Love Hacking and Video editing.")
 
-print("Daksh Jain
-Developer| Technical Consultant and an ardent fan of Real Madrid :)
-DakshJn")
+print("Daksh Jain Developer| Technical Consultant and an ardent fan of Real Madrid :) DakshJn")
 
-print("Dakshita agarwal
-Developer| Technical writer and Bug bounty hunter")
+print("Dakshita agarwal Developer| Technical writer and Bug bounty hunter")
 
-print("venkatesh pensalwar
-Developer| Love to learn new things and share knowledge.
-venkateshpensalwar")
+print("venkatesh pensalwar Developer| Love to learn new things and share knowledge. venkateshpensalwar")
 
-print("Aniket Chowdhury
-Developer| Machine Learning Analyst
-aniketbiprojit")
-print("Pulkit
-Developer| Machine Learning Analyst
-pulkitthakran")
+print("Aniket Chowdhury Developer| Machine Learning Analyst aniketbiprojit")
+
+print("Pulkit Developer| Machine Learning Analyst pulkitthakran")
