e0b9ca3f5fd16a3b31ba200ccfffccd6728f05b2
viteshmalhotra2608 <57283660+viteshmalhotra2608@users.noreply.github.com>
2020-10-13 23:56:35 +0530

    Update Names.py

--- Names.py
+++ Names.py
@@ -17,3 +17,5 @@ print("Pulkit Developer| Machine Learning Analyst pulkitthakran")
 print("Archit Developer| Machine Learning Analyst archit0712")
 
 print("Alok Tripathi | Competitive Programmer |Tripathi-Alok")
+
+print("Vitesh Developer| Machine Learning Analyst viteshmalhotra2608")
